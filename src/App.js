04eddac88e01cf3e9f235ff1c91b00a2708e28f7
HyunggiPark <phg9587@naver.com>
2023-01-22 20:30:14 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
     const [count, setCount] = useState(0)
     const onClick = () => {
         setCount((prevState) => prevState + 1)
     }
-    console.log('render')
+    console.log('i run all the time')
+    useEffect(() => {
+        console.log('CALL THE API...')
+    }, []) // 처음에 render 될 때 딱 1번만 실행되야하는 함수를 넣자.
     return (
         <div className="App">
             <h1>{count}</h1>
