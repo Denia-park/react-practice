8770503f59fe361982d35b4a09ff78e026446386
HyunggiPark <phg9587@naver.com>
2023-01-22 20:47:02 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import { useEffect, useState } from 'react'
 
 function App() {
     const [count, setCount] = useState(0)
+    const [keyword, setKeyword] = useState('')
+    const onChange = (event) => {
+        setKeyword(event.target.value)
+    }
     const onClick = () => {
         setCount((prevState) => prevState + 1)
     }
     console.log('i run all the time')
     useEffect(() => {
-        console.log('CALL THE API...')
+        console.log('i run only once')
     }, []) // 처음에 render 될 때 딱 1번만 실행되야하는 함수를 넣자.
+    useEffect(() => {
+        console.log('i run when "keyword" changes.')
+    }, [keyword])
+    useEffect(() => {
+        console.log('i run when "count" changes.')
+    }, [count])
+    useEffect(() => {
+        console.log('i run when "keyword && count" changes.')
+    }, [keyword, count])
+
     return (
         <div className="App">
+            <input value={keyword} onChange={onChange} type="text" placeholder={'Search here...'} />
             <h1>{count}</h1>
             <button onClick={onClick}>Click me</button>
         </div>
