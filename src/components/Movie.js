20e7b36d8e71895955dbbf2f8269fbda5f1657b8
HyunggiPark <phg9587@naver.com>
2023-01-24 17:15:30 +0900

    #7.9 Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({ id, title, coverImg, summary, genres }) {
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
             <img src={coverImg} alt={title} />
-            <p>{summary}</p>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map((genre) => (
                     <li key={genre}>{genre}</li>
